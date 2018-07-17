033715fd663827620fcc911cd117657dee7a6e32
Michael Meffie <mmeffie@sinenomine.net>
2018-07-16 21:24:21 -0400

    close the browser when done

--- visitcanton/2/visitcanton.py
+++ visitcanton/2/visitcanton.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
-from selenium import webdriver
+from selenium.webdriver import Firefox
 
-browser = webdriver.Firefox()
+browser = Firefox()
 browser.get('https://www.visitcanton.com/event-calendar/')
 element = browser.find_element_by_id('events-directory')
 text = element.text
 print(text)
+browser.close()
