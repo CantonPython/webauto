00a329dcf0632c9370cf04adf505754d843a6e8e
Michael Meffie <mmeffie@sinenomine.net>
2018-07-20 11:47:54 -0400

    use quit() not close()

--- visitcanton/2/visitcanton.py
+++ visitcanton/2/visitcanton.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 from selenium.webdriver import Firefox
 
 browser = Firefox()
@@ -6,4 +5,4 @@ browser.get('https://www.visitcanton.com/event-calendar/')
 element = browser.find_element_by_id('events-directory')
 text = element.text
 print(text)
-browser.close()
+browser.quit()
--- visitcanton/3/visitcanton.py
+++ visitcanton/3/visitcanton.py
@@ -1,14 +1,16 @@
-from __future__ import print_function
 from selenium.webdriver import Firefox, FirefoxOptions
 
 options = FirefoxOptions()
 options.set_headless()
 
-print('> starting firefox...')
-browser = Firefox(options=options)
-print('> getting the page...')
-browser.get('https://www.visitcanton.com/event-calendar/')
-element = browser.find_element_by_id('events-directory')
-text = element.text
-print(text)
-browser.close()
+try:
+    print('> starting firefox...')
+    browser = Firefox(options=options)
+    print('> getting the page...')
+    browser.get('https://www.visitcanton.com/event-calendar/')
+    element = browser.find_element_by_id('events-directory')
+    text = element.text
+    print('> events-directory text')
+    print(text)
+finally:
+    browser.quit()
